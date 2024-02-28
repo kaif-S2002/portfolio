7f49c1c68ecf645b42dfcde7d8ad4f6e832d2f43
kaif siddiqui <kaifs8998@gmail.com>
2024-02-29 01:16:30 +0530

    feat-created nav component

---
+++ app/components/nav.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const nav = () => {
+  return (
+    <>
+      <div className="nav-wrapper flex bg-zinc-900 py-6 px-12 justify-between items-center text-slate-100">
+        <div className="nav-logo">
+          <span className="flex justify-between items-center text-xl tracking-widest text-stone-400">
+            {"<Ks.dev/>"}
+          </span>
+        </div>
+        <div className="nav-items flex space-x-8">
+          <div className="nav-list">
+            <ul className="nav-ul flex text-base space-x-10 tracking-wider">
+              <li>Home</li>
+              <li>Projects</li>
+              <li>Skills</li>
+              <li>About Me</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <div className="nav-btn">
+            <button className=" text-base bg-stone-600 hover:bg-stone-500">
+              <a href="#">Resume</a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default nav;
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ weight: ["400"], subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -10,8 +11,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <html lang="en">
+        <body className={poppins.className}>{children}</body>
+      </html>
+    </>
   );
 }
