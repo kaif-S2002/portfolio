6518bb57c732bb92070c907e25b1ae6d1810693c
kaif siddiqui <kaifs8998@gmail.com>
2024-03-05 23:59:40 +0530

    feat-added about page

---
+++ app/components/about.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const about = () => {
+  return (
+    <>
+      <div className="about-wrapper">
+        <div className="about-text-area">
+          <div className="about-heading">
+            <h1>About</h1>
+          </div>
+          <div className="about-text">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus modi minima, nesciunt natus minus similique
+              dolores! Dolorem reiciendis itaque quasi ipsam repellendus saepe
+              nesciunt dignissimos architecto quos sed, illo quod!
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default about;
--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Nav from "./components/nav";
 import Main from "./components/main";
+import About from "./components/about";
 
 export default function Home() {
   return (
     <>
       <Nav />
       <Main />
+      <About />
     </>
   );
 }
