2c0542a430f15edc37ad67b13b167407e9c22347
kaif siddiqui <kaifs8998@gmail.com>
2024-03-06 23:59:13 +0530

    style-removed comments

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -16,13 +16,13 @@ const nav = () => {
                 <a href="#">Home</a>
               </li>
               <li>
+                <a href="#">About</a>
+              </li>
+              <li>
                 <a href="#">Projects</a>
               </li>
               <li>
                 <a href="#">Skills</a>
-              </li>
-              <li>
-                <a href="#">About Me</a>
               </li>
               <li>
                 <a href="#">Contact</a>
