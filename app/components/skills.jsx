afea42333b547d6a5d06098a52b17f223d1b1e73
kaif siddiqui <kaifs8998@gmail.com>
2024-03-11 23:55:54 +0530

    feat-added lists in skills section

--- app/components/skills.jsx
+++ app/components/skills.jsx
@@ -9,6 +9,24 @@ const skills = () => {
       <div className="skills-heading text-3xl text-stone-500 font-bold pb-10">
         <h1>{`<Skills />`}</h1>
       </div>
+
+      <div className="skills-wrapper flex justify-between items-start w-11/12 p-2 ">
+        <div className=" bg-zinc-950 rounded-xl h-full p-2">
+          <ul className="skills-list text-stone-500 space-y-2 w-full">
+            <li className="">Javascript</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>React JS</li>
+            <li>Tailwind CSS</li>
+          </ul>
+        </div>
+        <div className=" bg-zinc-950 rounded-xl h-full p-2">
+          <ul className="skills-list text-stone-500 space-y-2 w-full">
+            <li className="">Git & GitHub</li>
+            <li>VsCode</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
