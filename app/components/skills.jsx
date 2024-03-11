9ecbd425b9ab8c528e8516b5d3af768b18e52bbd
kaif siddiqui <kaifs8998@gmail.com>
2024-03-12 00:07:14 +0530

    refactor-changed skills wrapper hieght screen to fit

--- app/components/skills.jsx
+++ app/components/skills.jsx
@@ -4,15 +4,15 @@ const skills = () => {
   return (
     <div
       id="skills"
-      className="skills-wrapper flex flex-col justify-start items-start h-screen text-white px-16 py-28"
+      className="skills-wrapper flex flex-col justify-start items-start h-fit text-white px-20 pt-28 pb-16"
     >
       <div className="skills-heading text-3xl text-stone-500 font-bold pb-10">
         <h1>{`<Skills />`}</h1>
       </div>
 
-      <div className="skills-wrapper flex justify-between items-start w-11/12 p-2 ">
+      <div className="skills-wrapper flex justify-between items-start w-11/12 h-5/6">
         <div className=" bg-zinc-950 rounded-xl h-full p-2">
-          <ul className="skills-list text-stone-500 space-y-2 w-full">
+          <ul className="skills-list text-stone-500 space-y-6 w-full">
             <li className="">Javascript</li>
             <li>HTML</li>
             <li>CSS</li>
@@ -21,7 +21,7 @@ const skills = () => {
           </ul>
         </div>
         <div className=" bg-zinc-950 rounded-xl h-full p-2">
-          <ul className="skills-list text-stone-500 space-y-2 w-full">
+          <ul className="skills-list text-stone-500 space-y-6 w-full">
             <li className="">Git & GitHub</li>
             <li>VsCode</li>
           </ul>
