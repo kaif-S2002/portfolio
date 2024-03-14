6e248f65a298835c03f18552a18631750ea4db59
kaif siddiqui <kaifs8998@gmail.com>
2024-03-15 00:09:16 +0530

    refactor-added width in skills-sec

--- app/components/skills.jsx
+++ app/components/skills.jsx
@@ -11,50 +11,50 @@ const skills = () => {
       </div>
 
       <div className="skills-wrapper flex justify-between items-start w-11/12 h-5/6">
-        <div className="skills-sec flex flex-col bg-zinc-950 rounded-xl h-full p-2 gap-5">
+        <div className="skills-sec flex flex-col bg-zinc-950 rounded-xl w-4/12 h-full p-2 gap-5">
           <div className="skills-list text-stone-500 w-full">
             Javascript
-            <div className=" flex w-full h-1 bg-slate-600 rounded-lg">
-              <span className=" w-10/12 h-1 bg-sky-600 rounded-lg"></span>
+            <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
+              <span className=" w-10/12 h-1 bg-stone-600 rounded-lg"></span>
             </div>
           </div>
           <div className="skills-list text-stone-500 w-full">
             HTML
-            <div className=" flex w-full h-1 bg-slate-600 rounded-lg">
-              <span className=" w-8/12 h-1 bg-sky-600 rounded-lg"></span>
+            <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
+              <span className=" w-8/12 h-1 bg-stone-600 rounded-lg"></span>
             </div>
           </div>
           <div className="skills-list text-stone-500 w-full">
             CSS
-            <div className=" flex w-full h-1 bg-slate-600 rounded-lg">
-              <span className=" w-7/12 h-1 bg-sky-600 rounded-lg"></span>
+            <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
+              <span className=" w-7/12 h-1 bg-stone-600 rounded-lg"></span>
             </div>
           </div>
           <div className="skills-list text-stone-500 w-full">
             React JS
-            <div className=" flex w-full h-1 bg-slate-600 rounded-lg">
-              <span className=" w-9/12 h-1 bg-sky-600 rounded-lg"></span>
+            <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
+              <span className=" w-9/12 h-1 bg-stone-600 rounded-lg"></span>
             </div>
           </div>
           <div className="skills-list text-stone-500  w-full">
             Tailwind CSS
-            <div className=" flex w-full h-1 bg-slate-600 rounded-lg">
-              <span className=" w-7/12 h-1 bg-sky-600 rounded-lg"></span>
+            <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
+              <span className=" w-7/12 h-1 bg-stone-600 rounded-lg"></span>
             </div>
           </div>
         </div>
-        <div className="skills-sec bg-zinc-950 rounded-xl h-full p-2">
+        <div className="skills-sec bg-zinc-950 rounded-xl w-4/12 h-full p-2">
           <div className="skills-list text-stone-500 space-y-6 w-full">
             <div className="">
               Git & GitHub
-              <div className=" flex w-full h-1 bg-slate-600 rounded-lg">
-                <span className="  w-9/12 h-1 bg-sky-600 rounded-lg"></span>
+              <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
+                <span className="  w-9/12 h-1 bg-stone-600 rounded-lg"></span>
               </div>
             </div>
             <div>
               VsCode
-              <div className=" flex w-full h-1 bg-slate-600 rounded-lg">
-                <span className="  w-9/12 h-1 bg-sky-600 rounded-lg"></span>
+              <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
+                <span className="  w-9/12 h-1 bg-stone-600 rounded-lg"></span>
               </div>
             </div>
           </div>
