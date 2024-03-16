bb31fc59e5695aae08e4ed2e3f7673928e535e4a
kaif siddiqui <kaifs8998@gmail.com>
2024-03-16 23:46:56 +0530

    refactor-changed text color and added letter spacing

--- app/components/skills.jsx
+++ app/components/skills.jsx
@@ -11,32 +11,32 @@ const skills = () => {
       </div>
 
       <div className="skills-wrapper flex justify-between items-start w-11/12 h-5/6">
-        <div className="skills-sec flex flex-col bg-zinc-950 rounded-xl w-4/12 h-full p-2 gap-5">
-          <div className="skills-list text-stone-500 w-full">
+        <div className="skills-sec flex flex-col text-white bg-zinc-950 rounded-xl w-4/12 h-full p-2 gap-5">
+          <div className="skills-list w-full tracking-widest">
             Javascript
             <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
               <span className=" w-10/12 h-1 bg-stone-600 rounded-lg"></span>
             </div>
           </div>
-          <div className="skills-list text-stone-500 w-full">
+          <div className="skills-list w-full tracking-widest">
             HTML
             <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
               <span className=" w-8/12 h-1 bg-stone-600 rounded-lg"></span>
             </div>
           </div>
-          <div className="skills-list text-stone-500 w-full">
+          <div className="skills-list w-full tracking-widest">
             CSS
             <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
               <span className=" w-7/12 h-1 bg-stone-600 rounded-lg"></span>
             </div>
           </div>
-          <div className="skills-list text-stone-500 w-full">
+          <div className="skills-list w-full tracking-widest">
             React JS
             <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
               <span className=" w-9/12 h-1 bg-stone-600 rounded-lg"></span>
             </div>
           </div>
-          <div className="skills-list text-stone-500  w-full">
+          <div className="skills-list  w-full tracking-widest">
             Tailwind CSS
             <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
               <span className=" w-7/12 h-1 bg-stone-600 rounded-lg"></span>
@@ -44,7 +44,7 @@ const skills = () => {
           </div>
         </div>
         <div className="skills-sec bg-zinc-950 rounded-xl w-4/12 h-full p-2">
-          <div className="skills-list text-stone-500 space-y-6 w-full">
+          <div className="skills-list text-white space-y-6 w-full tracking-widest">
             <div className="">
               Git & GitHub
               <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
