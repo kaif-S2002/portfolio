b8ade070a8a327597ba1fa5a4444298e7fce66e7
kaif siddiqui <kaifs8998@gmail.com>
2024-03-17 23:58:14 +0530

    feat-added skills rendering method

--- app/components/skills.jsx
+++ app/components/skills.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Skills from "../data/skills";
 
 const skills = () => {
   return (
@@ -12,52 +13,16 @@ const skills = () => {
 
       <div className="skills-wrapper flex justify-between items-start w-11/12 h-5/6">
         <div className="skills-sec flex flex-col text-white bg-zinc-950 rounded-xl w-4/12 h-full p-2 gap-5">
-          <div className="skills-list w-full tracking-widest">
-            Javascript
-            <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
-              <span className=" w-10/12 h-1 bg-stone-600 rounded-lg"></span>
-            </div>
-          </div>
-          <div className="skills-list w-full tracking-widest">
-            HTML
-            <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
-              <span className=" w-8/12 h-1 bg-stone-600 rounded-lg"></span>
-            </div>
-          </div>
-          <div className="skills-list w-full tracking-widest">
-            CSS
-            <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
-              <span className=" w-7/12 h-1 bg-stone-600 rounded-lg"></span>
-            </div>
-          </div>
-          <div className="skills-list w-full tracking-widest">
-            React JS
-            <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
-              <span className=" w-9/12 h-1 bg-stone-600 rounded-lg"></span>
-            </div>
-          </div>
-          <div className="skills-list  w-full tracking-widest">
-            Tailwind CSS
-            <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
-              <span className=" w-7/12 h-1 bg-stone-600 rounded-lg"></span>
-            </div>
-          </div>
-        </div>
-        <div className="skills-sec bg-zinc-950 rounded-xl w-4/12 h-full p-2">
-          <div className="skills-list text-white space-y-6 w-full tracking-widest">
-            <div className="">
-              Git & GitHub
-              <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
-                <span className="  w-9/12 h-1 bg-stone-600 rounded-lg"></span>
+          {Skills.map((skill, id) => {
+            return (
+              <div key={id} className="skills-list w-full tracking-widest">
+                {skill.title}
+                <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
+                  <span className=" w-10/12 h-1 bg-stone-600 rounded-lg"></span>
+                </div>
               </div>
-            </div>
-            <div>
-              VsCode
-              <div className=" flex w-full h-1 bg-zinc-800 rounded-lg">
-                <span className="  w-9/12 h-1 bg-stone-600 rounded-lg"></span>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </div>
