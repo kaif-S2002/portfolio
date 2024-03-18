8175609a84e8a08f4f204433876c80bccc2a77c0
kaif siddiqui <kaifs8998@gmail.com>
2024-03-18 16:39:11 +0530

    refactor-updated navbar logo

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -7,7 +7,7 @@ const nav = () => {
       <div className="nav-wrapper flex bg-zinc-950 pt-6 pb-4 px-12 justify-between items-center text-slate-100 fixed w-screen z-10">
         <div className="nav-logo cursor-pointer">
           <span className="flex justify-between items-center text-xl font-semibold tracking-widest text-stone-500 select-none">
-            <a href="/">{"<Ks.dev/>"}</a>
+            <a href="/">{"<KS/>"}</a>
           </span>
         </div>
         <div className="nav-items flex space-x-8">
