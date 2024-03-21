47ec88508cfab7eeb9dd8c64140ad8478a612018
kaif siddiqui <kaifs8998@gmail.com>
2024-03-21 23:59:25 +0530

    feat-added more projects card

--- app/components/projects.jsx
+++ app/components/projects.jsx
@@ -7,8 +7,16 @@ const projects = () => {
         <div className="projects-heading text-3xl text-stone-500 font-semibold tracking-wider pb-10 ">
           <h1>{`<Projects/>`}</h1>
         </div>
-        <div className=" h-60 w-60 flex justify-center text-white text-lg p-2 rounded-xl bg-zinc-900">
-          News App
+        <div className="projects flex justify-evenly w-full">
+          <div className="projects-card h-60 w-60 flex justify-center text-white text-lg p-2 rounded-xl bg-zinc-900">
+            News App
+          </div>
+          <div className="projects-card h-60 w-60 flex justify-center text-white text-lg p-2 rounded-xl bg-zinc-900">
+            Ecommerce
+          </div>
+          <div className="projects-card h-60 w-60 flex justify-center text-white text-lg p-2 rounded-xl bg-zinc-900">
+            Portfolio
+          </div>
         </div>
       </section>
     </>
