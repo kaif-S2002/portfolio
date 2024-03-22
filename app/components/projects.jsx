59c3102936c1a3104dea930e997fbefc2d07aeb0
kaif siddiqui <kaifs8998@gmail.com>
2024-03-23 00:37:36 +0530

    feat-implemented hover effect on project cards

--- app/components/projects.jsx
+++ app/components/projects.jsx
@@ -7,14 +7,16 @@ const projects = () => {
         <div className="projects-heading text-3xl text-stone-500 font-semibold tracking-wider pb-10 ">
           <h1>{`<Projects/>`}</h1>
         </div>
-        <div className="projects flex justify-evenly w-full">
-          <div className="projects-card h-60 w-60 flex justify-center text-white text-lg p-2 rounded-xl bg-zinc-900">
+        <div className="projects flex justify-center gap-2 w-full">
+          <div
+            className={`projects-card news-card h-60 w-60 flex justify-center text-white text-lg p-2 rounded-xl bg-zinc-900 hover:w-6/12`}
+          >
             <h4>News App</h4>
           </div>
-          <div className="projects-card h-60 w-60 flex justify-center text-white text-lg p-2 rounded-xl bg-zinc-900">
+          <div className="projects-card ecommerce-card h-60 w-60 flex justify-center text-white text-lg p-2 rounded-xl bg-zinc-900 hover:w-6/12">
             <h4>Ecommerce</h4>
           </div>
-          <div className="projects-card h-60 w-60 flex justify-center text-white text-lg p-2 rounded-xl bg-zinc-900">
+          <div className="projects-card portfolio-card h-60 w-60 flex justify-center text-white text-lg p-2 rounded-xl bg-zinc-900 hover:w-6/12">
             <h4>Portfolio</h4>
           </div>
         </div>
