e1878d7b679844ee8c9af9d457fc5ee70d528afa
kaif siddiqui <kaifs8998@gmail.com>
2024-03-24 23:26:14 +0530

    refactor-changed flex property & text color

--- app/components/projects.jsx
+++ app/components/projects.jsx
@@ -3,20 +3,20 @@ import React from "react";
 const projects = () => {
   return (
     <>
-      <section className="projects-wrapper flex flex-col justify-start items-start px-16 py-32 h-screen">
-        <div className="projects-heading text-3xl text-stone-500 font-semibold tracking-wider pb-10 ">
+      <section className="projects-wrapper flex flex-col justify-center items-center px-16 py-32 h-screen">
+        <div className="projects-heading text-3xl text-stone-500 font-semibold tracking-wider pb-16 ">
           <h1>{`<Projects/>`}</h1>
         </div>
-        <div className="projects flex justify-center gap-2 w-full">
+        <div className="projects flex justify-center gap-2 text-stone-500 w-full">
           <div
-            className={`projects-card news-card h-60 w-60 flex justify-center text-white text-lg p-2 rounded-xl bg-zinc-900 hover:w-6/12`}
+            className={`projects-card news-card h-60 w-64 flex justify-center text-lg p-2 rounded-xl bg-zinc-950 hover:w-6/12 hover:scale-105 hover:shadow-lg`}
           >
             <h4>News App</h4>
           </div>
-          <div className="projects-card ecommerce-card h-60 w-60 flex justify-center text-white text-lg p-2 rounded-xl bg-zinc-900 hover:w-6/12">
+          <div className="projects-card ecommerce-card h-60 w-64 flex justify-center text-lg p-2 rounded-xl bg-zinc-950 hover:w-6/12 hover:scale-105 hover:shadow-lg">
             <h4>Ecommerce</h4>
           </div>
-          <div className="projects-card portfolio-card h-60 w-60 flex justify-center text-white text-lg p-2 rounded-xl bg-zinc-900 hover:w-6/12">
+          <div className="projects-card portfolio-card h-60 w-64 flex justify-center text-lg p-2 rounded-xl bg-zinc-950 hover:w-6/12 hover:scale-105 hover:shadow-lg">
             <h4>Portfolio</h4>
           </div>
         </div>
