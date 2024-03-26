a2e049fafa575a30c1ab778853fb10fbddd7369e
kaif siddiqui <kaifs8998@gmail.com>
2024-03-26 23:54:55 +0530

    feat-implemented projects icons & removed-scale transition

--- app/components/projects.jsx
+++ app/components/projects.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaNewspaper, FaCartShopping } from "react-icons/fa6";
+import { BsPersonWorkspace } from "react-icons/bs";
 
 const projects = () => {
   return (
@@ -10,17 +12,32 @@ const projects = () => {
         <div className="projects-heading text-3xl text-stone-500 font-semibold tracking-wider pb-16 ">
           <h1>{`<Projects/>`}</h1>
         </div>
-        <div className="projects flex justify-center gap-2 text-stone-500 w-full">
+        <div className="projects flex justify-center gap-3 text-stone-500 w-full">
           <div
-            className={`projects-card news-card h-64 w-64 flex justify-center text-lg p-2 rounded-xl bg-zinc-950 hover:w-6/12 hover:scale-105 hover:shadow-lg`}
+            className={`projects-card news-card flex flex-col justify-between items-center h-64 w-64 text-lg p-2 rounded-xl bg-zinc-950 hover:w-6/12 `}
           >
-            <h4>News App</h4>
+            <div className=" ">
+              <h4>News App</h4>
+            </div>
+            <div className=" pb-10 text-stone-600">
+              <FaNewspaper size={120} />
+            </div>
           </div>
-          <div className="projects-card ecommerce-card h-64 w-64 flex justify-center text-lg p-2 rounded-xl bg-zinc-950 hover:w-6/12 hover:scale-105 hover:shadow-lg">
-            <h4>Ecommerce</h4>
+          <div className="projects-card ecommerce-card h-64 w-64 flex flex-col justify-between items-center text-lg p-2 rounded-xl bg-zinc-950 hover:w-6/12">
+            <div className="">
+              <h4>Ecommerce</h4>
+            </div>
+            <div className=" pb-10 text-stone-600">
+              <BsPersonWorkspace size={120} />
+            </div>
           </div>
-          <div className="projects-card portfolio-card h-64 w-64 flex justify-center text-lg p-2 rounded-xl bg-zinc-950 hover:w-6/12 hover:scale-105 hover:shadow-lg">
-            <h4>Portfolio</h4>
+          <div className="projects-card portfolio-card h-64 w-64 flex flex-col justify-between items-center text-lg p-2 rounded-xl bg-zinc-950 hover:w-6/12 ">
+            <div className="">
+              <h4>Portfolio</h4>
+            </div>
+            <div className=" pb-10 text-stone-600">
+              <FaCartShopping size={120} />
+            </div>
           </div>
         </div>
       </section>
