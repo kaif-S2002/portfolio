52f12287de148fd1598d2c89af4c40a76ef9faf9
kaif siddiqui <kaifs8998@gmail.com>
2024-04-06 00:43:46 +0530

    feat-implemented new UI on skills section

--- app/components/skills.jsx
+++ app/components/skills.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import Skills from "../data/skills";
+import {
+  RiJavascriptFill,
+  RiHtml5Fill,
+  RiCss3Fill,
+  RiReactjsFill,
+} from "react-icons/ri";
+import { SiTailwindcss } from "react-icons/si";
+import { TbBrandVscode } from "react-icons/tb";
+import { FaGitAlt, FaGithub } from "react-icons/fa";
 
 const skills = () => {
   return (
     <section
       id="skills"
-      className="skills-wrapper flex flex-col justify-center items-center h-full text-white px-20 pt-36"
+      className="skills-wrapper flex flex-col justify-start items-center h-screen text-white px-20 pt-36"
     >
       <div className="skills-heading text-3xl text-stone-500 font-bold tracking-wider pb-10">
         <h1>{`<Skills />`}</h1>
       </div>
 
-      <div className="skills-wrapper flex justify-between items-start w-11/12 h-5/6">
+      {/* <div className="skills-wrapper flex justify-between items-start w-11/12 h-5/6">
         <div className="skills-sec flex flex-col text-white bg-zinc-950 rounded-xl w-4/12 h-full p-2 gap-5">
           {Skills.map((skill, id) => {
             return (
@@ -23,6 +32,63 @@ const skills = () => {
               </div>
             );
           })}
+        </div>
+      </div> */}
+      <div className="skills-list grid justify-center items-center gap-10 pt-12 text-stone-500 tracking-wider h-fit">
+        <div className=" flex flex-col justify-center items-center">
+          <i>
+            <RiJavascriptFill size={80} />
+          </i>
+          <h4>Javascript</h4>
+        </div>
+
+        <div className=" flex flex-col justify-center items-center">
+          <i>
+            <RiHtml5Fill size={80} />
+          </i>
+          <h4>HTML</h4>
+        </div>
+
+        <div className=" flex flex-col justify-center items-center">
+          <i>
+            <RiCss3Fill size={80} />
+          </i>
+          <h4>CSS</h4>
+        </div>
+
+        <div className=" flex flex-col justify-center items-center">
+          <i>
+            <RiReactjsFill size={80} />
+          </i>
+          <h4>React</h4>
+        </div>
+
+        <div className=" flex flex-col justify-center items-center">
+          <i>
+            <SiTailwindcss size={80} />
+          </i>
+          <h4>Tailwind CSS</h4>
+        </div>
+
+        <div className=" flex flex-col justify-center items-center">
+          <i>
+            <FaGitAlt size={80} />
+          </i>
+          <h4>Git</h4>
+        </div>
+
+        <div className=" flex flex-col justify-center items-center">
+          <i>
+            <FaGithub size={80} />
+          </i>
+          <h4>Github</h4>
+        </div>
+
+        <div className=" flex flex-col justify-center items-center">
+          <i>
+            <TbBrandVscode size={80} />
+          </i>
+          <h4>VsCode</h4>
         </div>
       </div>
     </section>
