847ec40b74ef8c865d358333fc0eed00a87d48b1
kaif siddiqui <kaifs8998@gmail.com>
2024-04-07 23:30:10 +0530

    feat-created contact section

---
+++ app/components/contact.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const contact = () => {
+  return (
+    <>
+      <section className="contact-wrapper flex flex-col justify-between items-center h-screen text-white px-20 pt-36">
+        <div className="contact-heading text-3xl text-stone-500 font-bold tracking-wider pb-10">
+          <h2>{`<Contact/>`}</h2>
+        </div>
+        <div className="greet-text text-lg text-stone-500 font-bold tracking-wider pb-1 pl-1 w-full border-b-2 border-stone-500">
+          <h1>Thanks for visiting.</h1>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default contact;
