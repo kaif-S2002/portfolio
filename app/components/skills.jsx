d020a5b648c2182111afc137690f4e62b7d8b7a1
kaif siddiqui <kaifs8998@gmail.com>
2024-04-12 23:37:18 +0530

    refactor- changed text size & added horizontal lines beside skills for better UI

--- app/components/skills.jsx
+++ app/components/skills.jsx
@@ -14,68 +14,73 @@ const skills = () => {
   return (
     <section
       id="skills"
-      className="skills-wrapper flex flex-col justify-start items-center h-screen text-white px-20 pt-32"
+      className="skills-wrapper flex flex-col justify-start items-center h-screen text-white px-20 pt-32 overflow-hidden"
     >
       <div className="skills-heading text-3xl text-stone-500 font-bold tracking-wider pb-10">
         <h1>{`<Skills />`}</h1>
       </div>
 
-      <div className="skills-list grid justify-center items-center gap-10 pt-8 text-stone-500 tracking-wider h-fit">
-        <div className=" flex flex-col justify-center items-center">
-          <i>
-            <RiJavascriptFill size={80} />
-          </i>
-          <h4>Javascript</h4>
-        </div>
+      <div className=" flex justify-between items-center gap-16 w-full">
+        <div className=" flex h-0.5 bg-stone-500 rounded-xl w-full"></div>
 
-        <div className=" flex flex-col justify-center items-center">
-          <i>
-            <RiHtml5Fill size={80} />
-          </i>
-          <h4>HTML</h4>
-        </div>
+        <div className="skills-list grid justify-center items-center gap-12 pt-8 text-stone-500 text-xs tracking-wide h-fit">
+          <div className=" flex flex-col justify-center items-center">
+            <i>
+              <RiJavascriptFill size={80} />
+            </i>
+            <h4>Javascript</h4>
+          </div>
 
-        <div className=" flex flex-col justify-center items-center">
-          <i>
-            <RiCss3Fill size={80} />
-          </i>
-          <h4>CSS</h4>
-        </div>
+          <div className=" flex flex-col justify-center items-center">
+            <i>
+              <RiHtml5Fill size={80} />
+            </i>
+            <h4>HTML</h4>
+          </div>
 
-        <div className=" flex flex-col justify-center items-center">
-          <i>
-            <RiReactjsFill size={80} />
-          </i>
-          <h4>React JS</h4>
-        </div>
+          <div className=" flex flex-col justify-center items-center">
+            <i>
+              <RiCss3Fill size={80} />
+            </i>
+            <h4>CSS</h4>
+          </div>
 
-        <div className=" flex flex-col justify-center items-center">
-          <i>
-            <TbBrandNextjs size={80} />
-          </i>
-          <h4>Next JS</h4>
-        </div>
+          <div className=" flex flex-col justify-center items-center">
+            <i>
+              <RiReactjsFill size={80} />
+            </i>
+            <h4>React JS</h4>
+          </div>
 
-        <div className=" flex flex-col justify-center items-center">
-          <i>
-            <SiTailwindcss size={80} />
-          </i>
-          <h4>Tailwind CSS</h4>
-        </div>
+          <div className=" flex flex-col justify-center items-center">
+            <i>
+              <TbBrandNextjs size={80} />
+            </i>
+            <h4>Next JS</h4>
+          </div>
 
-        <div className=" flex flex-col justify-center items-center">
-          <i>
-            <FaGitAlt size={80} />
-          </i>
-          <h4>Git</h4>
-        </div>
+          <div className=" flex flex-col justify-center items-center">
+            <i>
+              <SiTailwindcss size={80} />
+            </i>
+            <h4>Tailwind CSS</h4>
+          </div>
 
-        <div className=" flex flex-col justify-center items-center">
-          <i>
-            <FaGithub size={80} />
-          </i>
-          <h4>Github</h4>
+          <div className=" flex flex-col justify-center items-center">
+            <i>
+              <FaGitAlt size={80} />
+            </i>
+            <h4>Git</h4>
+          </div>
+
+          <div className=" flex flex-col justify-center items-center">
+            <i>
+              <FaGithub size={80} />
+            </i>
+            <h4>Github</h4>
+          </div>
         </div>
+        <div className=" flex h-0.5 bg-stone-500 rounded-xl w-full"></div>
       </div>
     </section>
   );
