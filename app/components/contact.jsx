aea762a9b0abaea3516118ef9f4b5dba8a88e54d
kaif siddiqui <kaifs8998@gmail.com>
2024-04-18 00:27:36 +0530

    refactor-updated top-padding of contact section

--- app/components/contact.jsx
+++ app/components/contact.jsx
@@ -7,13 +7,13 @@ function contact() {
     <>
       <section
         id="contact"
-        className="contact-wrapper flex flex-col text-white px-16 pt-28 m-auto h-fit"
+        className="contact-wrapper flex flex-col text-white px-16 pt-32 m-auto h-fit"
       >
         <div className="contact-heading flex justify-center items-center text-3xl text-stone-500 font-bold tracking-wider">
           <h1>{`<Contact />`}</h1>
         </div>
 
-        <div className="contact-items flex flex-col justify-center items-center text-stone-500 tracking-wider py-24">
+        <div className="contact-items flex flex-col justify-center items-center text-stone-500 tracking-wider pb-24 pt-20">
           <div className=" flex gap-20 pb-10">
             <div className="contact-num flex justify-center items-center gap-4 p-4 bg-zinc-950 rounded-xl">
               <i className="num-icon">
