8d4d4a1db97646862b22d5b9531a5121469032ff
kaif siddiqui <kaifs8998@gmail.com>
2024-04-23 23:59:47 +0530

    refactor-added project descriptions in portfolio and ecommerce card

--- app/components/projects.jsx
+++ app/components/projects.jsx
@@ -12,37 +12,47 @@ const projects = () => {
         <div className="projects-heading text-3xl text-stone-500 font-semibold tracking-wider pb-16 ">
           <h1>{`<Projects/>`}</h1>
         </div>
-        <div className="projects flex flex-col gap-20 text-stone-500 w-full">
+        <div className="projects flex flex-col gap-20 text-stone-500 w-full text-xl">
           <div
             id="news-card"
-            className={`projects-card news-card flex flex-col justify-between items-center h-64 w-7/12 text-lg p-2 rounded-xl bg-zinc-950 `}
+            className={`projects-card news-card flex justify-around items-center gap-6  h-64 w-7/12 p-2 rounded-xl bg-zinc-950`}
           >
-            <h4>News App</h4>
-            <FaNewspaper
-              size={120}
-              className="projects-icon mb-9 text-stone-600"
-            />
+            <FaNewspaper size={120} className="projects-icon text-stone-600" />
+            <div className=" flex flex-col w-6/12 gap-4">
+              <h4>News App</h4>
+              <p className=" text-sm">
+                Javascript, HTML, CSS, React JS, API's, JSON etc.
+              </p>
+            </div>
           </div>
 
           <div
             id="portfolio-card"
-            className={`projects-card portfolio-card flex flex-col justify-between items-center h-64 w-7/12 text-lg p-2 rounded-xl bg-zinc-950`}
+            className={`projects-card portfolio-card flex justify-around items-center gap-6 h-64 w-7/12 p-2 rounded-xl bg-zinc-950`}
           >
-            <h4>Portfolio</h4>
             <BsPersonWorkspace
               size={120}
-              className="projects-icon mb-9 text-stone-600"
+              className="projects-icon text-stone-600"
             />
+            <div className=" flex flex-col w-6/12 gap-4">
+              <h4>Portfolio</h4>
+              <p className=" text-sm">
+                Javascript, HTML, CSS, Next JS, Tailwind CSS etc.
+              </p>
+            </div>
           </div>
           <div
             id="ecommerce-card"
-            className={`projects-card ecommerce-card flex flex-col justify-between items-center h-64 w-7/12 text-lg p-2 rounded-xl bg-zinc-950 `}
+            className={`projects-card ecommerce-card flex justify-around items-center gap-6  h-64 w-7/12 p-2 rounded-xl bg-zinc-950`}
           >
-            <h4>Ecommerce</h4>
             <FaCartShopping
               size={120}
-              className="projects-icon mb-9 text-stone-600"
+              className="projects-icon text-stone-600"
             />
+            <div className=" flex flex-col w-6/12 gap-4">
+              <h4>Ecommerce</h4>
+              <p className=" text-sm">Arriving shortly !</p>
+            </div>
           </div>
         </div>
       </section>
