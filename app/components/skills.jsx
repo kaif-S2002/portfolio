b0b2003079f3cd359850419e3a47b90ef8476bdc
kaif siddiqui <kaifs8998@gmail.com>
2024-05-06 23:28:36 +0530

    fix-changed skills-card rendering method

--- app/components/skills.jsx
+++ app/components/skills.jsx
@@ -8,16 +8,29 @@ import {
 import { SiTailwindcss } from "react-icons/si";
 import { TbBrandNextjs } from "react-icons/tb";
 import { FaGitAlt, FaGithub } from "react-icons/fa";
+
 const skills = () => {
   const skillsData = [
-    { icon: <RiHtml5Fill size={40} />, title: "HTML" },
-    { icon: <RiCss3Fill size={40} />, title: "CSS" },
-    { icon: <RiJavascriptFill size={40} />, title: "Javascript" },
-    { icon: <RiReactjsFill size={40} />, title: "React JS" },
-    { icon: <TbBrandNextjs size={40} />, title: "Next JS" },
-    { icon: <SiTailwindcss size={40} />, title: "Tailwind CSS" },
-    // { icon: <FaGitAlt size={40} />, title: "Git" },
-    // { icon: <FaGithub size={40} />, title: "GitHub" },
+    {
+      frontend: {
+        title: "Front-end",
+        items: [
+          { icon: <RiHtml5Fill size={40} />, title: "HTML" },
+          { icon: <RiCss3Fill size={40} />, title: "CSS" },
+          { icon: <RiJavascriptFill size={40} />, title: "Javascript" },
+          { icon: <RiReactjsFill size={40} />, title: "React JS" },
+          { icon: <TbBrandNextjs size={40} />, title: "Next JS" },
+          { icon: <SiTailwindcss size={40} />, title: "Tailwind CSS" },
+        ],
+      },
+      devTools: {
+        title: "Dev Tools",
+        items: [
+          { icon: <FaGitAlt size={40} />, title: "Git" },
+          { icon: <FaGithub size={40} />, title: "GitHub" },
+        ],
+      },
+    },
   ];
 
   return (
@@ -27,16 +40,49 @@ const skills = () => {
     >
       <h1 className="skills-heading text-3xl text-stone-500 font-bold tracking-wider pb-6">{`<Skills />`}</h1>
 
-      <div className=" flex flex-col justify-center items-start bg-zinc-950 p-5 mt-12 rounded-xl text-stone-500">
-        <h2>Front-end</h2>
-        <div className=" h-6 w-0.5 bg-stone-600 mt-2 mb-4 ml-5"></div>
-        <div className="skills-list grid justify-center items-center gap-8 text-xs tracking-wide h-fit">
-          {skillsData.map((...[data]) => {
+      <div className="skills-cards flex justify-evenly items-start w-full h-fit mt-12 bg-zinc-950 rounded-xl">
+        <div className="flex flex-col justify-center items-start p-5 text-stone-500">
+          {skillsData.map((category) => {
             return (
               <>
-                <div className="skills-list-items grid grid-flow-col justify-start items-center">
-                  <i>{data.icon}</i>
-                  <h4 className=" ml-2">{data.title}</h4>
+                <div key={category.title}>
+                  <h2>{category.frontend.title}</h2>
+                  <div className="h-6 w-0.5 bg-stone-600 mt-2 mb-4 ml-4"></div>
+                  <div className="skills-list grid justify-center items-center gap-8 text-xs tracking-wide">
+                    {category.frontend.items.map((item, index) => (
+                      <div
+                        key={index}
+                        className="skills-list-items flex items-center"
+                      >
+                        <i>{item.icon}</i>
+                        <h4 className="ml-2">{item.title}</h4>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+
+        <div className="flex flex-col justify-start items-start p-5 text-stone-500">
+          {skillsData.map((category) => {
+            return (
+              <>
+                <div key={category.title}>
+                  <h2>{category.devTools.title}</h2>
+                  <div className="h-6 w-0.5 bg-stone-600 mt-2 mb-4 ml-4"></div>
+                  <div className="skills-list grid justify-center items-center gap-8 text-xs tracking-wide">
+                    {category.devTools.items.map((item, index) => (
+                      <div
+                        key={index}
+                        className="skills-list-items flex items-center"
+                      >
+                        <i>{item.icon}</i>
+                        <h4 className="ml-2">{item.title}</h4>
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </>
             );
