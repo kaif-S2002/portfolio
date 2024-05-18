7f15dfc7a9887c8dfb33584adbf0a26e7f117855
kaif siddiqui <kaifs8998@gmail.com>
2024-05-18 16:13:31 +0530

    refactor-changed color of all 'p' element in the footer section

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -6,10 +6,10 @@ const footer = () => {
     <>
       <footer
         id="footer"
-        className="footer-wrapper flex justify-between items-end mx-12 mb-8 text-stone-600 text-xs"
+        className="footer-wrapper flex justify-between items-end mx-12 mb-8 text-slate-100 text-xs"
       >
         <p>&copy;2024 Kaif Siddiqui. All rights reserved.</p>
-        <button className="top-btn pb-12 select-none">
+        <button className="top-btn pb-12 select-none text-stone-600">
           <a href="#home">&#8593;</a>
         </button>
         <p>Kaif siddiqui &middot; frontend web developer</p>
