2d70a73800a582d95135d11f47da90714fef5d93
kaif siddiqui <kaifs8998@gmail.com>
2024-05-21 00:25:41 +0530

    refactor-removed bold & letter-spacing property from about section heading

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -7,15 +7,15 @@ const about = () => {
     <>
       <section
         id="about"
-        className="about-wrapper flex flex-col text-white px-16 pt-36 m-auto h-fit overflow-hidden"
+        className="about-wrapper flex flex-col text-white px-16 pt-36 m-auto h-fit overflow-hidden tracking-wider"
       >
-        <div className="about-heading flex justify-center items-center text-3xl text-stone-500 font-bold tracking-wider">
+        <div className="about-heading flex justify-center items-center text-3xl text-stone-500">
           <h1>{`<About />`}</h1>
         </div>
-        <div className="about-text flex justify-between items-center w-full text-lg text-wrap tracking-widest pl-6 pt-20 pb-5">
+        <div className="about-text flex justify-between items-center w-full text-lg text-wrap pl-6 pt-20 pb-5">
           <p>
-            Hi, I'm <span>Kaif Siddiqui</span>, a self-taught frontend web
-            developer dedicated to creating an <span>engaging</span> and{" "}
+            Hi, I'm <span>Kaif Siddiqui</span>, a frontend web developer
+            dedicated to creating an <span>engaging</span> and{" "}
             <span>dynamic user experiences</span>. With a strong foundation in{" "}
             <span>HTML</span>, <span>CSS</span>, and <span>JavaScript</span>, I
             specialize in building <span>responsive</span> and{" "}
