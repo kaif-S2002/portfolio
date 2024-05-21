174cf92e884b5dbcf49cab104d7fe053c9007261
kaif siddiqui <kaifs8998@gmail.com>
2024-05-21 19:09:22 +0530

    refactor-increased padding bottom of 'top-btn' & changed color to 'stone-500' of footer section

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -9,7 +9,7 @@ const footer = () => {
         className="footer-wrapper flex justify-between items-end mx-12 mb-8 text-slate-100 text-xs"
       >
         <p>&copy;2024 Kaif Siddiqui. All rights reserved.</p>
-        <button className="top-btn pb-12 select-none text-stone-600">
+        <button className="top-btn pb-16 select-none text-stone-500">
           <a href="#home">&#8593;</a>
         </button>
         <p>Kaif siddiqui &middot; frontend web developer</p>
