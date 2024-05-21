a085f988cdd6959370c722bf01d1db98ba150d3f
kaif siddiqui <kaifs8998@gmail.com>
2024-05-21 16:31:41 +0530

    refactor-removed font-semibold property & changed color to 'stone-600' on heading of projects section

--- app/components/projects.jsx
+++ app/components/projects.jsx
@@ -10,7 +10,7 @@ const projects = () => {
         id="projects"
         className="projects-wrapper flex flex-col justify-center items-center px-16 pt-36 h-fit overflow-hidden"
       >
-        <div className="projects-heading text-3xl text-stone-500 font-semibold tracking-wider pb-16 ">
+        <div className="projects-heading text-3xl text-stone-600 tracking-wider pb-16 ">
           <h1>{`<Projects/>`}</h1>
         </div>
         <div className="projects flex flex-col gap-20 text-stone-500 w-full text-xl tracking-wider">
