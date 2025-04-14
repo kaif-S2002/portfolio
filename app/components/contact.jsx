5c3757eb4843946e41e6027e9982d5ec54c17de3
kaif siddiqui <kaifs8998@gmail.com>
2025-04-14 23:48:22 +0530

    refactor-Added 'use client' and removed extra fragments

--- app/components/contact.jsx
+++ app/components/contact.jsx
@@ -1,47 +1,46 @@
+"use client";
 import React from "react";
 
 function contact() {
   return (
-    <>
-      <section
-        id="contact"
-        className="contact-wrapper flex flex-col justify-evenly items-center text-2xl text-stone-500 px-16 tracking-wider overflow-hidden"
-      >
-        <div className=" text-3xl">
-          <h1>{`<Contact />`}</h1>
-        </div>
-        <div className="contact-links flex justify-evenly items-center w-full select-none">
-          <a
-            href="mailto:kaifs8998@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            E-mail
-          </a>
-          <a
-            href="http://www.linkedin.com/in/kaifsiddiqui02"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Linkedin
-          </a>
-          <a
-            href="https://github.com/kaif-S2002"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Github
-          </a>
-          <a
-            href="https://twitter.com/kaifsiddiqui__"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Twitter
-          </a>
-        </div>
-      </section>
-    </>
+    <section
+      id="contact"
+      className="contact-wrapper flex flex-col justify-evenly items-center text-2xl text-stone-500 px-16 tracking-wider overflow-hidden"
+    >
+      <div className=" text-3xl">
+        <h1>{`<Contact />`}</h1>
+      </div>
+      <div className="contact-links flex justify-evenly items-center w-full select-none">
+        <a
+          href="mailto:kaifs8998@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          E-mail
+        </a>
+        <a
+          href="http://www.linkedin.com/in/kaifsiddiqui02"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Linkedin
+        </a>
+        <a
+          href="https://github.com/kaif-S2002"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+        <a
+          href="https://twitter.com/kaifsiddiqui__"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Twitter
+        </a>
+      </div>
+    </section>
   );
 }
 
