92e595066b97687dd0787c0ffda52d7623a10091
kaif siddiqui <kaifs8998@gmail.com>
2025-04-15 23:43:34 +0530

    refactor-Added 'use client' and removed extra fragments

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,19 +1,18 @@
+"use client";
 import React from "react";
 
 const footer = () => {
   return (
-    <>
-      <footer
-        id="footer"
-        className="footer-wrapper flex justify-between items-end mx-12 mb-8 text-slate-100 text-xs"
-      >
-        <p>&copy;2024 Kaif Siddiqui. All rights reserved.</p>
-        <button className="top-btn pb-16 select-none text-stone-500">
-          <a href="#home">&#8593;</a>
-        </button>
-        <p>Kaif siddiqui &middot; frontend web developer</p>
-      </footer>
-    </>
+    <footer
+      id="footer"
+      className="footer-wrapper flex justify-between items-end mx-12 mb-8 text-slate-100 text-xs"
+    >
+      <p>&copy;2024 Kaif Siddiqui. All rights reserved.</p>
+      <button className="top-btn pb-16 select-none text-stone-500">
+        <a href="#home">&#8593;</a>
+      </button>
+      <p>Kaif siddiqui &middot; frontend web developer</p>
+    </footer>
   );
 };
 
